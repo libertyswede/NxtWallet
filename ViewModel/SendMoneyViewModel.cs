13e98f5434213a2c2e0abb2fa1b09dbaf2e769ec
Liberty Swede <libertyswede@outlook.com>
2016-03-16 18:44:27 +0100

    New viewmodel, SendMoneyViewModel

---
+++ ViewModel/SendMoneyViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace NxtWallet.ViewModel
+{
+    public class SendMoneyViewModel : BindableBase
+    {
+        private string _recipient;
+        private string _amount;
+        private string _message;
+
+        public string Recipient
+        {
+            get { return _recipient; }
+            set { SetProperty(ref _recipient, value); }
+        }
+
+        public string Amount
+        {
+            get { return _amount; }
+            set { SetProperty(ref _amount, value); }
+        }
+
+        public string Message
+        {
+            get { return _message; }
+            set { SetProperty(ref _message, value); }
+        }
+    }
+}
