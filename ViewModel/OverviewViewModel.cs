127907450d9947749783e4dbea0b74cb0ff15c63
Liberty Swede <libertyswede@outlook.com>
2016-03-20 21:04:53 +0100

    Bugfixes regarding updating transaction list

--- ViewModel/OverviewViewModel.cs
+++ ViewModel/OverviewViewModel.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 using GalaSoft.MvvmLight;
+using NxtLib.Local;
 using NxtWallet.Model;
 
 namespace NxtWallet.ViewModel
@@ -37,13 +38,22 @@ namespace NxtWallet.ViewModel
 
             Balance = "0.0";
             NxtAddress = walletRepository.NxtAccount.AccountRs;
+            Transactions = new ObservableCollection<Transaction>();
         }
 
         public void LoadFromRepository()
         {
             Balance = _walletRepository.Balance;
             var transactions = Task.Run(async () => await _walletRepository.GetAllTransactionsAsync()).Result;
-            Transactions = new ObservableCollection<Transaction>(transactions);
+            AppendTransactions(transactions);
+        }
+
+        private void AppendTransactions(IEnumerable<Transaction> transactions)
+        {
+            foreach (var transaction in transactions.Except(Transactions).OrderByDescending(t => t.Timestamp))
+            {
+                Transactions.Add(transaction);
+            }
         }
 
         public async Task LoadFromNxtServerAsync()
@@ -52,10 +62,14 @@ namespace NxtWallet.ViewModel
             await _walletRepository.SaveBalanceAsync(Balance);
             var lastTimestamp = Transactions.Any()
                 ? Transactions.Max(t => t.Timestamp)
-                : new DateTime(2013, 11, 24, 12, 0, 0, DateTimeKind.Utc);
-            var transactions = (await _nxtServer.GetTransactionsAsync(lastTimestamp)).ToList();
+                : Constants.EpochBeginning;
+
+            var transactions = (await _nxtServer.GetTransactionsAsync(lastTimestamp))
+                .Except(Transactions)
+                .ToList();
+            
             await _walletRepository.SaveTransactionsAsync(transactions);
-            Transactions = new ObservableCollection<Transaction>(transactions);
+            AppendTransactions(transactions);
         }
 
         private void NxtServer_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
