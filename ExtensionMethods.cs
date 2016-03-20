e7fe4949e9c5980de7ba0bb22ff585f62e7ea06a
Liberty Swede <libertyswede@outlook.com>
2016-03-20 19:36:45 +0100

    Amount to string extension method

---
+++ ExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using NxtLib;
+
+namespace NxtWallet
+{
+    public static class ExtensionMethods
+    {
+        public static string ToFormattedString(this Amount amount)
+        {
+            var formatted = amount.Nxt.ToString("##.#########");
+            return formatted;
+        }
+    }
+}
