579bc59080252283cffe43e32d51db5cf6be5002
Liberty Swede <libertyswede@outlook.com>
2016-03-27 16:48:14 +0200

    Fix for line endings in transaction list UI

---
+++ Src/NxtWallet/StringNewlineConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace NxtWallet
+{
+    public class StringNewlineConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var theString = value.ToString();
+            var newLineIndex = theString.IndexOfAny(new[] {'\n', '\r'});
+            if (newLineIndex != -1)
+            {
+                theString = theString.Substring(0, newLineIndex);
+                theString = theString.TrimEnd() + " ...";
+            }
+            return theString;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
