017907d7954d08a2de48e844e01234c66a4ee224
Liberty Swede <libertyswede@outlook.com>
2016-04-02 17:35:26 +0200

    Basic receive money view

--- Src/NxtWallet/ViewModel/ReceiveMoneyViewModel.cs
+++ Src/NxtWallet/ViewModel/ReceiveMoneyViewModel.cs
@@ -1,5 +1,7 @@
-﻿using GalaSoft.MvvmLight;
+﻿using Windows.UI.Xaml.Media.Imaging;
+using GalaSoft.MvvmLight;
 using NxtWallet.Model;
+using ZXing;
 
 namespace NxtWallet.ViewModel
 {
@@ -7,6 +9,7 @@ namespace NxtWallet.ViewModel
     {
         private string _nxtAddress;
         private string _publicKey;
+        private WriteableBitmap _nxtAddressQr;
 
         public string NxtAddress
         {
@@ -20,10 +23,19 @@ namespace NxtWallet.ViewModel
             set { Set(ref _publicKey, value); }
         }
 
+        public WriteableBitmap NxtAddressQr
+        {
+            get { return _nxtAddressQr; }
+            set { Set(ref _nxtAddressQr, value); }
+        }
+
         public ReceiveMoneyViewModel(IWalletRepository walletRepository)
         {
             NxtAddress = walletRepository.NxtAccount.AccountRs;
             PublicKey = walletRepository.NxtAccount.PublicKey.ToHexString();
+
+            var writer = new BarcodeWriter {Format = BarcodeFormat.QR_CODE};
+            NxtAddressQr = (WriteableBitmap) writer.Write(NxtAddress).ToBitmap();
         }
     }
 }
