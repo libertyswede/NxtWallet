9c23ac2527f9f210bf2783db736a251aa75d558a
Liberty Swede <libertyswede@outlook.com>
2016-04-10 21:45:39 +0200

    TX id is now clickable

--- Src/NxtWallet/ViewModel/TransactionDetailViewModel.cs
+++ Src/NxtWallet/ViewModel/TransactionDetailViewModel.cs
@@ -6,11 +6,27 @@ namespace NxtWallet.ViewModel
     public class TransactionDetailViewModel : ViewModelBase
     {
         private Transaction _transaction;
+        private string _transactionLink;
 
         public Transaction Transaction
         {
             get { return _transaction; }
-            set { Set(ref _transaction, value); }
+            set
+            {
+                Set(ref _transaction, value);
+                UpdateTransactionLink();
+            }
+        }
+
+        public string TransactionLink
+        {
+            get { return _transactionLink; }
+            set { Set(ref _transactionLink, value); }
+        }
+
+        private void UpdateTransactionLink()
+        {
+            TransactionLink = $"https://nxtportal.org/transactions/{Transaction.NxtId}";
         }
     }
 }
