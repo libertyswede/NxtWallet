3cb44f5af63849b599bf6abe3686d30cf2867dc5
Liberty Swede <libertyswede@outlook.com>
2016-06-04 22:30:49 +0200

    Truncate messages after 50 characters

--- Src/NxtWallet/StringNewlineConverter.cs
+++ Src/NxtWallet/StringNewlineConverter.cs
@@ -7,16 +7,34 @@ namespace NxtWallet
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
+            const int maxLength = 50;
+
             if (value == null)
                 return string.Empty;
 
             var theString = value.ToString();
-            var newLineIndex = theString.IndexOfAny(new[] {'\n', '\r'});
-            if (newLineIndex != -1)
+            var firstNewLineIndex = theString.IndexOfAny(new[] { '\n', '\r' });
+            int stringLengthCutoff = -1;
+
+            if (theString.Length > maxLength && firstNewLineIndex == -1)
             {
-                theString = theString.Substring(0, newLineIndex);
+                stringLengthCutoff = maxLength;
+            }
+            else if (theString.Length <= maxLength && firstNewLineIndex > -1)
+            {
+                stringLengthCutoff = firstNewLineIndex;
+            }
+            else if (theString.Length > maxLength && firstNewLineIndex > -1)
+            {
+                stringLengthCutoff = Math.Min(Math.Min(maxLength, theString.Length), firstNewLineIndex);
+            }
+
+            if (stringLengthCutoff > -1)
+            {
+                theString = theString.Substring(0, stringLengthCutoff);
                 theString = theString.TrimEnd() + " ...";
             }
+
             return theString;
         }
 
